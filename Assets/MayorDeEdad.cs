a967c42634f2d267c917cddfe05c5974d672be83
facuzylberberg <101568863+facuzylberberg@users.noreply.github.com>
2022-04-11 19:27:28 -0300

    Initial commit

---
+++ Assets/MayorDeEdad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayorDeEdad : MonoBehaviour
+{
+    public int edadusuario;
+    public Text miTexto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edadusuario >= 18)
+        {
+            miTexto.text = "Usted es mayor de edad";
+            Debug.Log("Usted es mayor de edad");
+
+        }
+
+        else
+        {
+            miTexto.text = "Usted no es mayor de edad";
+            Debug.Log("Usted no es mayor de edad");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
